dc7ec890c789f8b78e185e21c16118ba8a7151b5
vinypaulino <vinydos3@gmail.com>
2017-10-24 12:09:51 -0300

    Configuracoes Iniciasi
    Configuracoes iniciais do Projeto com Hibernate

---
+++ src/br/com/vinypaulino/Produto.java
@@ -0,0 +1,39 @@
+package br.com.vinypaulino;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nome;
+	private int quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
